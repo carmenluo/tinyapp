c59089739d1f29d4f34f33fc37fab881686f5c1f
Carmen <sdluojm@gmail.com>
2019-09-02 18:17:11 +0000

    add more pages"

--- express_server.js
+++ express_server.js
@@ -10,6 +10,12 @@ const urlDatabse = {
 app.get("/",(req, res) => {
   res.send('Hello');
 });
+app.get('/urls.json',(req, res) => {
+  res.json(urlDatabse);
+});
+app.get("/hello",(req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
 
 app.listen(PORT, () => {
   console.log(`Expample app listening on port ${PORT}!`);
