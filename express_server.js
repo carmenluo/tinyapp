eb7acc1ad05f4528a15d0a46b651de7c739fda49
Carmen <sdluojm@gmail.com>
2019-09-02 19:04:01 +0000

    add ejs as view engine

--- express_server.js
+++ express_server.js
@@ -2,13 +2,17 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 app.set('view engine','ejs');
-const urlDatabse = {
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
 app.get("/",(req, res) => {
   res.send('Hello');
+});
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 app.get('/urls.json',(req, res) => {
   res.json(urlDatabse);
