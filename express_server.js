8169ff8473ffbe8fcc2298eded1bebcb60415eb8
Carmen <sdluojm@gmail.com>
2019-09-05 08:32:54 +0000

    register and login function

--- express_server.js
+++ express_server.js
@@ -13,47 +13,63 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@ee.com",
+    password: "12345"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
 
 app.get("/", (req, res) => {
   res.send('Hello');
 });
 app.get("/urls", (req, res) => {
-  let templateVars = {};
-  console.log(req.cookies["username"]);
-  if (req.cookies["username"]) {
-    templateVars = {
-      urls: urlDatabase,
-      username: req.cookies["username"]
-    }
-  } else {
-    templateVars = {
-      urls: urlDatabase
-    }
-  }
+  let templateVars = checkCookies(req, res);
   res.render("urls_index", templateVars);
 });
+app.get("/login", (req, res) => {
+  const templateVars = { users: users };
+  res.render("login", templateVars);
+});
 app.post("/login", (req, res) => {
-  console.log(req.body.username);
-  res.cookie("username", req.body.username);
-  res.redirect("/urls");
+  for (let user in users) {
+    if (users[user].email === req.body.email) {
+      if (users[user].password === req.body.password) {
+        res.cookie('user_id', user);
+        //   req.session.user_id = user;
+        res.redirect("/urls");
+        break;
+      } else {
+        res.statusCode = 403;
+        res.send(`Error Code: 403. Password not matching. Please double check;`);
+      }
+    } else {
+      res.statusCode = 403;
+      res.send(`Error Code: 403. Record not found. Please double check;`);
+    }
+  }
 });
 app.post("/logout", (req, res) => {
-  res.clearCookie("username");
+  res.clearCookie("user_id");
   res.redirect("/urls");
 });
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = checkCookies(req, res);
+  res.render("urls_new", templateVars);
 })
 app.post("/urls/:shortURL", (req, res) => {
-  console.log(req.body);
   urlDatabase[req.params.shortURL] = req.body.longURL;
   res.redirect("/urls");
 })
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect("/urls");
-  // res.render("urls_index", templateVars);
-
 })
 app.post("/urls", (req, res) => {
   let shortURL = generateRandomString();
@@ -62,13 +78,39 @@ app.post("/urls", (req, res) => {
 })
 app.get("/urls/:shortURL", (req, res) => {
   let shortURL = req.params.shortURL
-  let templateVars = { shortURL: shortURL, longURL: urlDatabase[shortURL] };
+  let templateVars = { shortURL: shortURL, longURL: urlDatabase[shortURL], useremail: emailLookup(req.cookies["user_id"]) };
   res.render("urls_show", templateVars);
 })
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
+//Rigister
+app.get("/register", (req, res) => {
+  const templateVars = { users: users };
+  res.render("register", templateVars);
+});
+app.post("/register", (req, res) => {
+  let id = generateRandomString();
+  let email = req.body.email;
+  let password = req.body.password;
+  if (!email) {
+    res.statusCode = 400;
+    res.send(`Error code: 400 Email address can't not be empty`);
+  };
+  for (let user in users) {
+    if (users[user].email === req.body.email) {
+      res.statusCode = 400;
+      res.send(`Error code: 400 Email Register already!`)
+    }
+  };
+    let user = { id, email, password };
+    users[id] = user;
+    res.cookie('user_id', id);
+    // req.session.user_id = user;
+    // break;
+    res.redirect('/urls');
+  });
 // app.post
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabse);
@@ -87,3 +129,25 @@ app.get('/fetch', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Expample app listening on port ${PORT}!`);
 });
+
+function checkCookies(req, res) {
+  let templateVars = {};
+  if (req.cookies["user_id"]) {
+    templateVars = {
+      urls: urlDatabase,
+      useremail: emailLookup(req.cookies["user_id"])
+    }
+  } else {
+    templateVars = {
+      urls: urlDatabase
+    }
+  }
+  return templateVars;
+}
+function emailLookup(user_id) {
+  for (let user in users) {
+    if (users[user].id === user_id) {
+      return users[user].email;
+    }
+  }
+};
